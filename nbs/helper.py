adef746d7eb8715f8ff074b8ebb1bdf4f6a7976e
guillaume <guillaume.ramelet@gmail.com>
2025-01-04 22:34:17 +0100

    marche avec azure openai

--- nbs/helper.py
+++ nbs/helper.py
@@ -6,6 +6,9 @@ __all__ = [
     "get_gemini_api_key",
     "get_openai_api_key",
     "get_google_projectID",
+    "get_google_auth_file",
+    "get_azure_openai_keys",
+    "get_azure_llm",
     "get_gemini_llm",
     "get_gemini_llamaindex_llm",
     "get_vertex_llm",
@@ -38,7 +41,39 @@ def get_google_projectID():
     return google_projectID
 
 
-# %% helper.ipynb 4
+def get_google_auth_file():
+    load_env()
+    google_auth_file = os.getenv("GOOGLE_AUTH_FILE")
+    return google_auth_file
+
+
+def get_azure_openai_keys():
+    load_env()
+    azure_api_key = os.getenv("AZURE_API_KEY")
+    azure_endpoint = os.getenv("AZURE_ENDPOINT")
+    azure_api_version = os.getenv("AZURE_API_VERSION")
+    return azure_api_key, azure_endpoint, azure_api_version
+
+
+# %% helper.ipynb 5
+from llama_index.llms.azure_openai import AzureOpenAI
+from llama_index.embeddings.azure_openai import AzureOpenAIEmbedding
+from llama_index.core import Settings
+
+
+def get_azure_llm(engine="gpt-4o"):
+    AZURE_API_KEY, AZURE_ENDPOINT, AZURE_API_VERSION = get_azure_openai_keys()
+    llm = AzureOpenAI(
+        engine=engine,
+        api_key=AZURE_API_KEY,
+        azure_endpoint=AZURE_ENDPOINT,
+        api_version=AZURE_API_VERSION,
+    )
+    Settings.llm = llm
+    return llm
+
+
+# %% helper.ipynb 8
 import google.generativeai as genai
 
 
@@ -48,7 +83,7 @@ def get_gemini_llm(model="gemini-1.5-flash"):
     return llm
 
 
-# %% helper.ipynb 7
+# %% helper.ipynb 11
 from llama_index.llms.gemini import Gemini
 from llama_index.core import Settings
 
@@ -60,12 +95,13 @@ def get_gemini_llamaindex_llm(model="models/gemini-1.5-flash"):
     return llm
 
 
-# %% helper.ipynb 10
+# %% helper.ipynb 14
 from llama_index.core import Settings
 from llama_index.llms.vertex import Vertex
+from google.oauth2 import service_account
 
 
-def get_vertex_llm(model="models/gemini-1.5-flash"):
+def get_vertex_llm(model="gemini-1.5-flash-001"):
 
     # Set up necessary variables
     credentials = {
@@ -73,12 +109,22 @@ def get_vertex_llm(model="models/gemini-1.5-flash"):
         "api_key": get_gemini_api_key(),
     }
 
+    # filename = get_google_auth_file()
+    # credentials_service: service_account.Credentials = (
+    #     service_account.Credentials.from_service_account_file(filename)
+    # )
+
     # Create an instance of the Vertex class
     llm = Vertex(
         model=model,
         project=credentials["project_id"],
         credentials=credentials,
-        context_window=4096,
+        context_window=100000,
     )
+    # llm = Vertex(
+    #     model=model,
+    #     project=credentials_service.project_id,
+    #     credentials=credentials_service,
+    #     context_window=100000,    )
     Settings.llm = llm
     return llm
