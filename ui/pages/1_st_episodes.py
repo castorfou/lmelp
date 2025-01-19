3f6b144aebb38b815a4a7d0fc8fa2349ba2ba973
guillaume <guillaume.ramelet@gmail.com>
2025-01-19 19:12:38 +0100

    affichage episodes

--- ui/pages/1_st_episodes.py
+++ ui/pages/1_st_episodes.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from git import Repo
 import os
+import sys
 
 
 def get_git_root(path):
@@ -10,13 +11,34 @@ def get_git_root(path):
 
 project_root = get_git_root(os.getcwd())
 
+# Ajouter le chemin du répertoire 'nbs' à sys.path
+sys.path.append(os.path.abspath(os.path.join(project_root, "nbs")))
+
+from mongo_episode import Episodes
+import pandas as pd
+
+
+@st.cache_data  # 👈 Add the caching decorator
+def get_episodes():
+    episodes = Episodes()
+    episodes_df = pd.DataFrame(
+        [episode.to_dict() for episode in episodes.get_entries()]
+    )
+    episodes_df["date"] = episodes_df["date"].dt.strftime("%Y/%m/%d")
+    episodes_df["duree (min)"] = (episodes_df["duree"] / 60).round(1)
+    episodes_df.drop(
+        columns=["url_telechargement", "audio_rel_filename", "type", "duree"],
+        inplace=True,
+    )
+    return episodes_df
+
 
 def afficher_episodes():
     st.write("### Épisodes")
     st.write("Liste des épisodes du Masque et la Plume.")
+    episodes_df = get_episodes()
     # Ajoutez ici le code pour afficher les épisodes
+    st.dataframe(episodes_df, use_container_width=True)
 
 
 afficher_episodes()
-
-st.write(project_root)
