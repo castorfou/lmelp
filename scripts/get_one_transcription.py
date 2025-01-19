f8c651044be070eeba53cd9f0f2476537bce1e53
guillaume <guillaume.ramelet@gmail.com>
2025-01-19 11:39:45 +0100

    Add duration tracking and warnings suppression to transcription script

--- scripts/get_one_transcription.py
+++ scripts/get_one_transcription.py
@@ -1,6 +1,8 @@
 import sys
 import os
 import dbus
+import time
+import warnings
 
 # Ajouter le chemin du répertoire 'nbs' à sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../nbs")))
@@ -9,6 +11,25 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../nbs"
 # et avec l'interpreter python whisper
 
 from mongo_episode import Episodes
+
+
+def print_duree_traitement(start_time, end_time):
+    # Calculer la durée
+    duration = end_time - start_time
+
+    # Convertir la durée en heures, minutes et secondes
+    hours, remainder = divmod(duration, 3600)
+    minutes, seconds = divmod(remainder, 60)
+
+    # Afficher la durée formatée
+    if hours > 0:
+        print(
+            f"Le traitement a pris {int(hours)} heures, {int(minutes)} minutes et {int(seconds)} secondes."
+        )
+    else:
+        print(
+            f"Le traitement a pris {int(minutes)} minutes et {int(seconds)} secondes."
+        )
 
 
 def main():
@@ -22,6 +43,7 @@ def main():
 
     # Prévenir la mise en veille
     cookie = interface.Inhibit("my_script", "Long running process")
+    print("Mise en veille deactivee")
 
     episodes = Episodes()
     print(episodes)
@@ -29,14 +51,23 @@ def main():
     if len(miss_transcriptions) > 0:
         # on prend le dernier
         miss_transcription = miss_transcriptions[-1]
+        print("On va recuperer la transcription de l'episode \n")
+        print(miss_transcription)
         try:
             # Votre traitement long ici
+            start_time = time.time()
             miss_transcription.set_transcription(verbose=True)
+            end_time = time.time()
+            print_duree_traitement(start_time=start_time, end_time=end_time)
+
         finally:
             # Réactiver la mise en veille normale
+            print("Mise en veille normale reactivee")
             interface.UnInhibit(cookie)
     print(episodes)
 
 
 if __name__ == "__main__":
+    # Ignorer les warnings
+    warnings.filterwarnings("ignore")
     main()
