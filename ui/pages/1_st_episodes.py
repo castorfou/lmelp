1c46b738794507bd8bb08ef41495416d853e4814
guillaume <guillaume.ramelet@gmail.com>
2025-01-26 21:48:43 +0100

    Add date selection feature and French locale support in episode display

--- ui/pages/1_st_episodes.py
+++ ui/pages/1_st_episodes.py
@@ -11,6 +11,14 @@ add_to_sys_path()
 
 from mongo_episode import Episodes
 import pandas as pd
+from datetime import datetime
+
+import locale
+
+# Définir la locale en français
+locale.setlocale(locale.LC_TIME, "fr_FR.UTF-8")
+
+DATE_FORMAT = "%d %b %Y"
 
 
 @st.cache_data  # 👈 Add the caching decorator
@@ -49,4 +57,40 @@ def afficher_episodes():
     st.dataframe(episodes_df, use_container_width=True)
 
 
-afficher_episodes()
+def afficher_un_episode(episodes_df):
+    # Widget de sélection de date
+    selected_date = st.date_input(
+        "Sélectionnez une date",
+        min_value=datetime(1958, 1, 1),
+        max_value=datetime.today(),
+    )
+
+    # Convertir la date sélectionnée en chaîne de caractères au format yyyy/mm/dd
+    selected_date_str = selected_date.strftime("%Y/%m/%d")
+
+    # Filtrer le DataFrame pour trouver la ligne correspondant à la date sélectionnée
+    episode = episodes_df[episodes_df["date"] == selected_date_str]
+
+    if not episode.empty:
+        episode = episode.iloc[0]
+        st.write(f"### {episode['titre']}")
+        st.write(f"Date: {selected_date.strftime(DATE_FORMAT)}")
+
+        st.write(f"Durée: {episode['duree (min)']} minutes")
+        st.write(f"Description: {episode['description']}")
+        st.write(f"Transcription: {episode['transcription']}")
+    else:
+        st.write("Aucun épisode trouvé pour cette date.")
+
+
+# Sidebar pour la navigation
+option = st.sidebar.selectbox(
+    "Options", ["Visualiser l'ensemble des épisodes", "Visualiser un épisode"]
+)
+
+episodes_df = get_episodes()
+# Afficher le contenu en fonction de la sélection
+if option == "Visualiser l'ensemble des épisodes":
+    afficher_episodes()
+elif option == "Visualiser un épisode":
+    afficher_un_episode(episodes_df)
