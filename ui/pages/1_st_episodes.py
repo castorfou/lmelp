cb11f39cc87f511a2d87887dbabf1b2491913190
guillaume <guillaume.ramelet@gmail.com>
2025-01-29 02:29:25 +0100

    mots par transcription

--- ui/pages/1_st_episodes.py
+++ ui/pages/1_st_episodes.py
@@ -21,6 +21,7 @@ import pandas as pd
 from datetime import datetime
 
 import locale
+import plotly.express as px
 
 # Définir la locale en français
 locale.setlocale(locale.LC_TIME, "fr_FR.UTF-8")
@@ -55,10 +56,9 @@ def print_episodes_info(episodes_df):
     st.write(f"Transcriptions manquantes : {transcriptions_missing}")
 
 
-def afficher_episodes():
+def afficher_episodes(episodes_df):
     st.write("### Épisodes")
     st.write("Liste des épisodes du Masque et la Plume.")
-    episodes_df = get_episodes()
     print_episodes_info(episodes_df)
     # Ajoutez ici le code pour afficher les épisodes
     st.dataframe(episodes_df, use_container_width=True)
@@ -75,23 +75,69 @@ def afficher_un_episode(episodes_df):
         episode = episode.iloc[0]
         st.write(f"### {episode['titre']}")
         selected_date_dt = datetime.strptime(selected_date, "%Y/%m/%d")
-        st.write(f"Date: {selected_date_dt.strftime(DATE_FORMAT)}")
+        st.write(f"**Date**: {selected_date_dt.strftime(DATE_FORMAT)}")
 
-        st.write(f"Durée: {episode['duree (min)']} minutes")
-        st.write(f"Description: {episode['description']}")
-        st.write(f"Transcription: {episode['transcription']}")
+        st.write(f"**Durée**: {episode['duree (min)']} minutes")
+        st.write(f"**Description**: {episode['description']}")
+        st.write(f"**Transcription**: {episode['transcription']}")
     else:
         st.write("Aucun épisode trouvé pour cette date.")
 
 
+def nb_mots_transcription(episodes_df):
+
+    st.write("### Nombre de mots par transcription")
+    # Compter le nombre de mots dans chaque transcription
+
+    episodes_df = episodes_df.copy()
+
+    # Calculer le nombre de mots par minute
+    episodes_df["mots_par_minute"] = (
+        episodes_df["transcription"].apply(lambda x: len(x.split()))
+        / episodes_df["duree (min)"]
+    )
+
+    # Transformer la date en format dd/mm/yyyy
+    episodes_df["date_formatted"] = episodes_df["date"].apply(
+        lambda x: datetime.strptime(x, "%Y/%m/%d")
+    )
+    episodes_df["date_formatted"] = episodes_df["date_formatted"].apply(
+        lambda x: x.strftime("%d/%m/%Y")
+    )
+
+    # Trier les données par nombre de mots par minute décroissant
+    episodes_df = episodes_df.sort_values(by="mots_par_minute", ascending=False)
+
+    # Créer le graphique interactif
+    fig = px.bar(
+        episodes_df,
+        x="date_formatted",
+        y="mots_par_minute",
+        custom_data=["date_formatted", "titre", "mots_par_minute", "duree (min)"],
+        labels={"mots_par_minute": "Mots par Minute"},
+        title="Nombre de Mots par Minute par Épisode",
+    )
+
+    # # Mettre à jour les informations de survol
+    fig.update_traces(
+        hovertemplate="<b>Date:</b> %{customdata[0]}<br><b>Titre:</b> %{customdata[1]}<br><b>Mots par Minute:</b> %{customdata[2]:.1f}<br><b>Duree en Minute:</b> %{customdata[3]:.1f}"
+    )
+
+    # Afficher le graphique
+    st.plotly_chart(fig, use_container_width=True)
+
+
 # Sidebar pour la navigation
 option = st.sidebar.selectbox(
-    "Options", ["Visualiser l'ensemble des épisodes", "Visualiser un épisode"]
+    "Options",
+    ["Tous les épisodes", "Visualiser un épisode", "# mots par transcription"],
 )
 
 episodes_df = get_episodes()
 # Afficher le contenu en fonction de la sélection
-if option == "Visualiser l'ensemble des épisodes":
-    afficher_episodes()
+if option == "Tous les épisodes":
+    afficher_episodes(episodes_df)
 elif option == "Visualiser un épisode":
     afficher_un_episode(episodes_df)
+elif option == "# mots par transcription":
+    nb_mots_transcription(episodes_df)
