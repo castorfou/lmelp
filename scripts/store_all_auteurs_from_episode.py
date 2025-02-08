a73c724265f60b3a95bc399f5f1f8b9644748ee2
guillaume <guillaume.ramelet@gmail.com>
2025-02-08 14:21:02 +0100

    isolate logic in a function

--- scripts/store_all_auteurs_from_episode.py
+++ scripts/store_all_auteurs_from_episode.py
@@ -2,6 +2,7 @@ import sys
 import os
 import argparse
 import datetime
+import pandas as pd
 
 # Ajouter le chemin du répertoire 'nbs' à sys.path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../nbs")))
@@ -11,6 +12,61 @@ from mongo_auteur import Auteur, AuthorChecker
 
 # il faudra executer ce script dans le repo (utilisation de la lib git)
 # et avec l'interpreter python whisper
+
+
+def ajoute_auteurs(episode: Episode):
+    affiche_episode = f"""
+    Date: {episode.date.strftime("%d %b %Y")}
+    Titre: {episode.titre}
+    Description: {episode.description}
+
+    """
+    print(affiche_episode)
+
+    auteurs = episode.get_all_auteurs()
+    print(f"La liste des auteurs : {auteurs}\n")
+
+    ac = AuthorChecker(episode)
+    auteur_traitement_df = pd.DataFrame(
+        columns=["auteur_corrige", "detection", "existait_en_base", "anomalie"]
+    )
+    analyse_dict = {}
+
+    for auteur in auteurs:
+        auteur_corrige_dict = ac.check_author(auteur, return_details=True)
+        auteur_corrige = (
+            auteur_corrige_dict["author_corrected"]
+            if auteur_corrige_dict["author_corrected"]
+            != auteur_corrige_dict["author_original"]
+            else None
+        )
+        detection = auteur_corrige_dict["source"]
+        anomalie = True if auteur_corrige_dict["author_corrected"] is None else False
+        existait_en_base = (
+            False
+            if anomalie
+            else Auteur(auteur_corrige_dict["author_corrected"]).exists()
+        )
+        auteur_traitement_df.loc[auteur] = [
+            auteur_corrige,
+            detection,
+            existait_en_base,
+            anomalie,
+        ]
+        if not anomalie:
+            aut = Auteur(auteur_corrige)
+            aut.keep()
+        else:
+            analyse_dict[auteur] = {
+                "analyse": auteur_corrige_dict["analyse"],
+                "score": auteur_corrige_dict["score"],
+            }
+
+    print(auteur_traitement_df)
+    if len(analyse_dict) > 0:
+        print("\nAnalyse des anomalies\n")
+    for auteur, details in analyse_dict.items():
+        print(f"{auteur} -> {details['analyse']} (score: {details['score']})")
 
 
 if __name__ == "__main__":
@@ -34,17 +90,4 @@ if __name__ == "__main__":
         print(f"Episode du {date.strftime('%d/%m/%Y')} n'existe pas")
         sys.exit(1)
 
-    print(episode)
-
-    auteurs = episode.get_all_auteurs()
-    print(auteurs)
-
-    ac = AuthorChecker(episode)
-
-    for auteur in auteurs:
-        auteur_corrige = ac.check_author(auteur)
-        if auteur_corrige is not None:
-            print(f"{auteur} -> {auteur_corrige}")
-            aut = Auteur(auteur_corrige)
-            print(f"Est-ce que {aut.nom} existe ? {aut.exists()}")
-            aut.keep()
+    ajoute_auteurs(episode)
