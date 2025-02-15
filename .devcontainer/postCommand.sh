93d0cde11ebef5fca7523222abea9ab819a0db0a
guillaume <guillaume.ramelet@gmail.com>
2025-02-15 18:17:45 +0000

    locale fr

--- .devcontainer/postCommand.sh
+++ .devcontainer/postCommand.sh
@@ -5,4 +5,6 @@ uv venv
 source .venv/bin/activate
 sudo apt update
 sudo apt install -y libdbus-1-dev libdbus-glib-1-dev cmake 
+sudo apt install -y locales
+sudo sed -i 's/^# *\(fr_FR.UTF-8\)/\1/' /etc/locale.gen
 uv pip install -r .devcontainer/requirements.txt
