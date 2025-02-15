68e7cc281ea0a638d96d48e0306c82fef41f935c
guillaume <guillaume.ramelet@gmail.com>
2025-02-15 22:03:59 +0000

    to call from host

--- ui/lmelp_ui.sh
+++ ui/lmelp_ui.sh
@@ -1,4 +1,7 @@
 #!/bin/bash 
+# to be launched externally with 
+# docker exec -u vscode loving_bose /workspaces/lmelp/ui/lmelp_ui.sh
+export PATH="$HOME/.local/bin:$PATH"
 export GPG_TTY=$(tty)
 pushd /workspaces/lmelp/
 streamlit run ui/lmelp.py
