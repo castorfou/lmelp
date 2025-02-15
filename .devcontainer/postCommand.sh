777a5d2bf4346533a2e06245bcd82c46365f60d1
guillaume <guillaume.ramelet@gmail.com>
2025-02-15 18:18:47 +0000

    pre-commit

--- .devcontainer/postCommand.sh
+++ .devcontainer/postCommand.sh
@@ -8,3 +8,4 @@ sudo apt install -y libdbus-1-dev libdbus-glib-1-dev cmake
 sudo apt install -y locales
 sudo sed -i 's/^# *\(fr_FR.UTF-8\)/\1/' /etc/locale.gen
 uv pip install -r .devcontainer/requirements.txt
+pre-commit install
