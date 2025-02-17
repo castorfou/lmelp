dbd98b969ad0a68b38f27274e0259b82c576330f
guillaume <guillaume.ramelet@michelin.com>
2025-02-17 15:47:15 +0000

    under wsl

---
+++ .devcontainer/wsl_lmelp/postCommand.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+apt update
+apt install -y libdbus-1-dev libdbus-glib-1-dev cmake 
+apt install -y locales
+sed -i 's/^# *\(fr_FR.UTF-8\)/\1/' /etc/locale.gen
+dpkg-reconfigure locales -f noninteractive
+pip install --upgrade pip
+pip install -r .devcontainer/requirements.txt
+pre-commit install
+git config --system --add safe.directory '*'
+ 
+# Create Streamlit credentials file to avoid the email prompt
+mkdir -p ~/.streamlit
+cat << 'EOF' > ~/.streamlit/credentials.toml
+[general]
+email = "your_email@example.com"
+EOF
