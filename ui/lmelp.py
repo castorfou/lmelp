76daf8ad7632327c6a90f380837feb1151dc51ed
guillaume <guillaume.ramelet@gmail.com>
2025-02-17 21:21:46 +0000

    affichage des auteurs

--- ui/lmelp.py
+++ ui/lmelp.py
@@ -22,9 +22,9 @@ st.page_link("lmelp.py", label="Home", icon="🏠")
 
 # https://fonts.google.com/icons?selected=Material+Symbols+Outlined:music_note:FILL@0;wght@400;GRAD@0;opsz@24&icon.query=music&icon.size=24&icon.color=%235f6368
 
-st.page_link("pages/1_st_episodes.py", label="episodes", icon=":material/music_note:")
-st.page_link("pages/2_st_auteurs.py", label="auteurs", icon=":material/person:")
-st.page_link("pages/3_st_livres.py", label="livres", icon=":material/menu_book:")
+st.page_link("pages/1_episodes.py", label="episodes", icon=":material/music_note:")
+st.page_link("pages/2_auteurs.py", label="auteurs", icon=":material/person:")
+st.page_link("pages/3_livres.py", label="livres", icon=":material/menu_book:")
 
 st.write("## Contenu (a mettre sous forme de cartes)")
 st.write(f"Auteurs tbd")
