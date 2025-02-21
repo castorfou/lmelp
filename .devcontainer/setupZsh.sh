4754c02a0c3e70afc717d72f048ef097d79fd391
guillaume <guillaume.ramelet@gmail.com>
2025-02-21 22:03:57 +0100

    oh-my-zsh powerline

--- .devcontainer/setupZsh.sh
+++ .devcontainer/setupZsh.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+cd ~
+rm -rf .oh-my-zsh
+
+sudo apt install -y fonts-powerline
+
+sh -c "$(wget -O- https://github.com/deluan/zsh-in-docker/releases/download/v1.2.1/zsh-in-docker.sh)" -- \
+    -p git \
+    -p ssh-agent \
+    -p 'history-substring-search' \
+    -p https://github.com/zsh-users/zsh-autosuggestions \
+    -p https://github.com/zsh-users/zsh-completions
+echo '[[ ! -f ~/.p10k.zsh ]] || source ~/.p10k.zsh' >> .zshrc
+# wget https://raw.githubusercontent.com/castorfou/dotfiles/refs/heads/framework/home/guillaume/.p10k.sh -O .p10k.zsh
+wget https://raw.githubusercontent.com/chbroecker/dotfiles/main/zsh/.p10k.zsh -O .p10k.zsh
+exec zsh
