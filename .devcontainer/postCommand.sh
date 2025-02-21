a25c9030827bef0549bdf40253bd9c03491d1455
guillaume <guillaume.ramelet@gmail.com>
2025-02-21 18:31:19 +0000

    update def config devcontainer

--- .devcontainer/postCommand.sh
+++ .devcontainer/postCommand.sh
@@ -7,6 +7,8 @@ sudo dpkg-reconfigure locales -f noninteractive
 pip install --upgrade pip
 pip install -r .devcontainer/requirements.txt
 pre-commit install
+pre-commit autoupdate
+
 sudo git config --system --add safe.directory '*'
 
 # Create Streamlit credentials file to avoid the email prompt
